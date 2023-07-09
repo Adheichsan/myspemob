f811427520fdd54d1655fe9af66d294520419dcc
Adheichsan <melodyslsbll90@gmail.com>
2023-07-10 02:46:09 +0700

    pemmob lagi, pemmob lagi

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable, camel_case_types
+
 import 'package:flutter/material.dart';
 import 'package:myspemob/assets/constant.dart';
 import 'package:myspemob/pages/homepage.dart';
